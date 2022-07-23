c54d1711b83c9c94f8a9e9a5f0164f9908c9120f
harshil sharma <harshilsharmaa0@gmail.com>
2022-07-23 22:12:37 +0530

    Get Food Items in given range of calories

--- controllers/foodItem.js
+++ controllers/foodItem.js
@@ -4,8 +4,8 @@ exports.addFoodItem = async (req, res) => {
     try {
 
         console.log(req.body);
-        
-        if(!req.body.name || !req.body.calories || !req.body.protein || !req.body.carbs || !req.body.fat || !req.body.itemWeight || !req.body.acceptedUnit) {
+
+        if (!req.body.name || !req.body.calories || !req.body.protein || !req.body.carbs || !req.body.fat || !req.body.itemWeight || !req.body.acceptedUnit) {
             return res.status(400).json({
                 success: false,
                 message: 'Please provide all required fields'
@@ -46,6 +46,113 @@ exports.getFoodItems = async (req, res) => {
             message: 'FoodItems retrieved successfully',
             foodItems
         })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+            success: false
+        })
+    }
+}
+
+exports.deleteFoodItem = async (req, res) => {
+    
+        try {
+    
+            const foodItem = await FoodItem.findByIdAndDelete(req.params.id);
+    
+            res.status(200).json({
+                success: true,
+                message: 'FoodItem deleted successfully',
+                foodItem
+            })
+    
+        } catch (error) {
+            res.status(500).json({
+                message: error.message,
+                success: false
+            })
+        }
+}
+
+
+// to find all the subset of a given array of numbers that sum to a given number
+function subsetSumToK(arr, n, givenCalories) {
+    let sum = 0;
+    let i = 0;
+    let j = 0;
+    let result = [];
+
+    let count = 0;
+    while(i <n&& j<n){
+        sum+=arr[j].calories;
+        
+        if(sum>=givenCalories-100 && sum<=givenCalories+100){
+            let temp = [i,j-1];
+            result.push(temp);
+        }
+        
+        else if(sum>givenCalories+100){
+            sum-=arr[j].calories;
+            while(i<=j){
+                sum-=arr[i].calories;
+                i++;
+                if(sum>=givenCalories-100 && sum<=givenCalories+100){
+                    let temp =[i,j-1];
+                    result.push(temp);
+                }
+            }
+        }
+        j++;
+        
+    }
+    return result;
+}
+
+
+exports.getFoodItemsByCalories = async (req, res) => {
+
+    try {
+
+
+        const foodItems = await FoodItem.find({}).sort({calories: 1});
+
+        let foodItemsToReturn = [];
+
+        let a = subsetSumToK(foodItems, foodItems.length, Number(req.params.givenCalories));
+
+        a.map(item => {
+            let f = item[0];
+            let s = item[1];
+            let temp = {};
+            let protein = 0;
+            let calories = 0;
+            
+
+            // food item with their protein level
+            for(let i=f; i<=s; i++){
+                protein += foodItems[i].protein;
+                calories += foodItems[i].calories;
+                temp[foodItems[i].name] = foodItems[i].protein;
+            }
+        
+            temp.TotalProtein = protein;
+            temp.TotalCalories = calories;
+            temp.proteinPercent = (protein/calories)*100;
+            foodItemsToReturn.push(temp);
+        })
+
+
+        // sort the meal having protein level between 20 to 30 percent of the total calories if its possible.
+        foodItemsToReturn.sort((a, b) => {
+            return b.proteinPercent - a.proteinPercent;
+        })
+
+        res.status(200).json({
+            success: true,
+            message: 'FoodItems retrieved successfully',
+            foodItemsToReturn
+        })
+
     } catch (error) {
         res.status(500).json({
             message: error.message,
--- routes/foodItem.js
+++ routes/foodItem.js
@@ -3,12 +3,16 @@ const router = express.Router();
 
 const {
     addFoodItem,
-    getFoodItems
+    getFoodItems,
+    getFoodItemsByCalories,
+    deleteFoodItem
 } = require('../controllers/foodItem');
 
 router.route('/add').post(addFoodItem);
+router.route('/getFoodItemsByCalories/:givenCalories').get(getFoodItemsByCalories);
 
 
 router.route('/get').get(getFoodItems);
+router.route('/delete/:id').delete(deleteFoodItem);
 
 module.exports = router;
